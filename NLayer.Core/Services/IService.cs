991aee4d8a999b033be9177e0882052436ffae9c
Hakan GUL <hakanngul@icloud.com>
2022-02-19 23:00:55 +0300

    Core Layer Tamamlandı.

---
+++ NLayer.Core/Services/IService.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayer.Core.Services;
+
+public interface IService<T> where T : class
+{
+    Task<T> GetByIdAsync(int id);
+
+    Task<IEnumerable<T>> GetAllAsync();
+
+    IEnumerable<T> Where(Expression<Func<T, bool>> predicate);
+
+    Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+
+    Task AddAsync(T entity);
+
+    Task AddRangeAsync(IEnumerable<T> entities);
+
+    Task UpdateAsync(T entity);
+
+    Task RemoveAsync(T entity);
+
+    Task RemoveRangeAsync(IEnumerable<T> entities);
+}
